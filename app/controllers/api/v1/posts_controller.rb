d61970fa07cec99c245a3bedf245eff856e45079
Adrian Bautista <abautista@kitchensurfing.com>
2016-02-01 18:18:38 +0800

    Enqueue job in API and strip whitespace

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,11 +2,13 @@ class Api::V1::PostsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    message = TextMessage.create!(
-      body: params['Body'],
+    text_message = TextMessage.create!(
+      body: params['Body'].strip!,
       phone_number: params['From'],
       data: params.to_json
     )
+    TextMessageProcessorJob.peform_later(text_message)
+
     head :created
   end
 end
